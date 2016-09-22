3a2c6c0f5f56a8f50130327281a42961050efc3b
dapeng126 <zhangdapeng89@126.com>
2016-09-22 08:02:16 +0800

    System.out.prinln to Log.i

--- app/src/androidTest/java/com/gowhich/androidsqlite/PersonDao2Test.java
+++ app/src/androidTest/java/com/gowhich/androidsqlite/PersonDao2Test.java
@@ -3,6 +3,7 @@ package com.gowhich.androidsqlite;
 import android.app.Application;
 import android.content.ContentValues;
 import android.test.ApplicationTestCase;
+import android.util.Log;
 
 import com.gowhich.androidsqlite.dao.PersonDao2;
 import com.gowhich.androidsqlite.service.PersonService2;
@@ -25,18 +26,18 @@ public class PersonDao2Test extends ApplicationTestCase<Application> {
     public void insertDb(){
         PersonService2 service = new PersonDao2(getContext());
         ContentValues values = new ContentValues();
-        values.put("name","张三");
+        values.put("name","李四");
         values.put("address","北京");
         values.put("sex","不祥");
         boolean flag = service.addPerson(values);
-        System.out.println("insertDb ------>" + flag);
+        Log.i("PersonDao2Test","insertDb ------>" + flag);
     }
 
     @Test
     public void deleteDb(){
         PersonService2 service = new PersonDao2(getContext());
-        boolean flag = service.deletePerson(" id = ? ", new String[]{"2"});
-        System.out.println("deleteDb ------>" + flag);
+        boolean flag = service.deletePerson(" id = ? ", new String[]{"3"});
+        Log.i("PersonDao2Test","deleteDb ------>" + flag);
     }
 
     @Test
@@ -46,22 +47,22 @@ public class PersonDao2Test extends ApplicationTestCase<Application> {
         values.put("name","王五");
         values.put("address","上海");
         values.put("sex","男");
-        boolean flag = service.updatePerson(values, " id = ? ", new String[]{" 2 "});
-        System.out.println("updateDb ------>" + flag);
+        boolean flag = service.updatePerson(values, " id = ? ", new String[]{" 6 "});
+        Log.i("PersonDao2Test","updateDb ------>" + flag);
     }
 
     @Test
     public void viewDB(){
         PersonService2 service = new PersonDao2(getContext());
-        Map<String, String> map = service.viewPerson(" id = ? ", new String[]{"2"});
-        System.out.println("viewDB ------>" + map.toString());
+        Map<String, String> map = service.viewPerson(" id = ? ", new String[]{"6"});
+        Log.i("PersonDao2Test","viewDB ------>" + map.toString());
     }
 
     @Test
     public void listDB(){
         PersonService2 service = new PersonDao2(getContext());
         List<Map<String, String>> list = service.listPerson(null, null);
-        System.out.println("listDB ------>" + list.toString());
+        Log.i("PersonDao2Test","listDB ------>" + list.toString());
     }
 
 }
